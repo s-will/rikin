e67f3489de746e3142d29ee2d134abfc19975a07
Sebastian Will <swill@csail.mit.edu>
2014-12-18 11:37:08 +0100

    Cleanup

---
+++ src/basin_pruning_info.cc
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "basin_pruning_info.hh"
+
+std::ostream &
+BasinPruningInfo::write(std::ostream &out,
+			double min_contribution, 
+			size_t num_input_basins) const {
+    
+    std::vector<double> row;
+    row.resize(num_input_basins);
+    
+    for (BasinPruningInfo::contributions_t::const_iterator it=contributions_.begin(); 
+	 contributions_.end()!=it; ++it) {
+	if (it->second >= min_contribution) { 
+	    row[it->first] = it->second;
+	}
+    }
+    row[orig_index_]=1;
+
+    for (size_t i=0; i<row.size(); i++) {
+	std::ostringstream entry;
+	entry.precision(3);
+	entry << row[i];
+	
+	if (i!=0) out<< '\t';
+	out << entry.str();
+    }
+    
+    return out;
+}
+
+BasinPruningInfo::BasinPruningInfo(size_t index)
+    : orig_index_(index),
+      contributions_() // no contributions to new basin
+{
+}
+
+void
+BasinPruningInfo::merge_in_basin(size_t orig_index, double contribution, const BasinPruningInfo &bpi, double min_contribution) {
+
+    if (contribution<min_contribution) return;
+
+    // index must not contribute to the basin already
+    assert(contributions_.find(orig_index) == contributions_.end());
+    
+    contributions_[orig_index] = contribution; 
+    
+    for ( contributions_t::const_iterator it = bpi.contributions_.begin();
+	  bpi.contributions_.end() != it; ++it ) {
+	
+	if ( contribution * it->second >= min_contribution ) {
+	    contributions_[it->first] += contribution * it->second;
+	}
+    }
+}
---
+++ src/basin_pruning_info.hh
@@ -0,0 +1,65 @@
+#ifndef BASIN_PRUNING_INFO_HH
+#define BASIN_PRUNING_INFO_HH
+
+#include <iosfwd>
+#include <tr1/unordered_map>
+#include <limits>
+
+//! Additional information about a basin
+class BasinPruningInfo {
+    
+    size_t orig_index_;
+    
+    typedef std::tr1::unordered_map<size_t,double> contributions_t;
+    contributions_t contributions_;
+    
+public:
+
+    /**
+     * @brief "empty" constructor
+     *
+     * @param index original basin index ("input index")
+     *
+     * No other, dissolved basins contribute to this basin yet
+     */
+    BasinPruningInfo(size_t index=std::numeric_limits<size_t>::max());
+
+    /** 
+     * @brief update info after merging in a basin 
+     * 
+     * @param orig_index original index of merged in basin
+     * @param contribution proportion of the merged in basin's contribution
+     * @param bpi pruning info of merged in basin
+     * @param min_contribution minimum contribution that is recorded (as single term in summations)
+     *
+     * @note the merged in basin itself is going to be dissolved
+     */    
+    void
+    merge_in_basin(size_t orig_index, double contribution, 
+		   const BasinPruningInfo &bpi,
+		   double min_contribution);
+    
+    // /**
+    //  * @brief sparsify
+    //  * @param min_contrib minimum contribution
+    //  *
+    //  * Remove all stored contributions less than min_contribution 
+    //  */
+    // void
+    // sparsify(double min_contrib);
+    
+    /**
+     * @brief access original index
+     */
+    size_t 
+    orig_index() const {return orig_index_;} 
+    
+    //! @brief write to stream
+    std::ostream &
+    write(std::ostream &out, double min_contribution, size_t num_input_basins) const;
+
+    
+};
+
+
+#endif // BASIN_PRUNING_INFO_HH
