9a0af9c55b6fcada2b6a838e1a19c2526c559cf3
Sebastian Will <swill@csail.mit.edu>
2017-08-02 14:00:24 +0200

    Fix linking

--- src/rrikin_enum.cc
+++ src/rrikin_enum.cc
@@ -5,7 +5,7 @@
    and assumes simple hybridization at each site.  In the model, we
    assume that single structures outside of the hybridization sites
    and the hybridization sites itself are each equilibrated.
-   
+
    We start by defining classes for the computation of hybrid
    partition functions and joint probabilities for two unpaired sites.
 */
@@ -34,14 +34,14 @@ main(int argc, char **argv)
     stopwatch.start("total");
 
     gengetopt_args_info args_info;
-    
+
     // get options (call gengetopt command line parser)
     if (cmdline_parser (argc, argv, &args_info) != 0)
 	exit(1) ;
 
     bool homodimer=args_info.homodimer_given;
     bool antisense=args_info.antisense_given;
-    
+
     size_t expected_sequences=(homodimer||antisense)?1:2;
 
     if (homodimer && antisense) {
@@ -50,18 +50,18 @@ main(int argc, char **argv)
 	cmdline_parser_free(&args_info);
 	exit(1);
     }
-    
+
     if ( args_info.inputs_num != expected_sequences ) {
 	std::cerr << "Expect "<<expected_sequences<<" sequence(s) on command line."<<std::endl;
 	cmdline_parser_print_help();
 	cmdline_parser_free(&args_info);
 	exit(1);
     }
-    
+
     std::string seqA = args_info.inputs[0];
     HybEnsModel::normalize_RNA_sequence(seqA);
     std::string seqB = "";
-    
+
     if (homodimer) {
 	seqB = seqA;
 	HybEnsModel::reverse(seqB);
@@ -72,18 +72,18 @@ main(int argc, char **argv)
 	seqB = args_info.inputs[1];
 	HybEnsModel::normalize_RNA_sequence(seqB);
     }
-        
+
     // set some global variables for Vienna libRNA
     dangles=2;
-    
+
     const double max_hyb_energy   = args_info.max_hyb_energy_arg;
     const double max_total_energy = args_info.max_total_energy_arg;
-    const size_t maxsitesize = 
+    const size_t maxsitesize =
 	(args_info.max_hyb_length_arg>=0)
 	? args_info.max_hyb_length_arg
 	: std::max(seqA.length(),seqB.length());
-    
-    const size_t maxsitesize_diff = 
+
+    const size_t maxsitesize_diff =
 	(args_info.max_hyb_length_diff_arg>=0)
 	? args_info.max_hyb_length_diff_arg
 	: std::max(seqA.length(),seqB.length());
@@ -92,16 +92,16 @@ main(int argc, char **argv)
     size_t region_endA=seqA.length();
     size_t region_startB=1;
     size_t region_endB=seqB.length();
-    
+
     parse_region(args_info.regionA_arg,region_startA,region_endA);
     parse_region(args_info.regionB_arg,region_startB,region_endB);
-    
-    const size_t span = 
+
+    const size_t span =
 	args_info.span_arg>=0
 	? args_info.span_arg
 	: std::numeric_limits<size_t>::max();
-    
-    const size_t window = 
+
+    const size_t window =
 	args_info.span_arg>=0
 	? args_info.span_arg*2
 	: std::numeric_limits<size_t>::max();
@@ -110,7 +110,7 @@ main(int argc, char **argv)
     bool enum_double_sites        = ! args_info.no_double_sites_given;
     bool binary        = args_info.binary_given;
     bool add_open_state        = args_info.add_open_state_given;
-    
+
     // ------------------------------------------------------------
     // enumerate states
     stopwatch.start("init_model");
@@ -120,7 +120,7 @@ main(int argc, char **argv)
 			   << "; region A " << region_startA << "-" << region_endA
 			   << "; region B " << region_startB << "-" << region_endB
 			   <<")" << std::endl;
-    
+
     HybEnsModel model(seqA,seqB,
 		      maxsitesize,
 		      maxsitesize_diff,
@@ -138,7 +138,7 @@ main(int argc, char **argv)
 			       max_hyb_energy,
 			       max_total_energy,
 			       binary);
-    
+
     //if (verbose) stopwatch.print_info(std::cerr);
 
     stopwatch.start("enumerate");
@@ -146,12 +146,12 @@ main(int argc, char **argv)
     if (verbose) std::cerr << "Enumerate states ( max hyb length "<<maxsitesize
 			   << ", max hyb length diff "<<maxsitesize_diff
 			   << ", max ss hyb energy " <<max_hyb_energy
-			   << ", max total energy " << max_total_energy 
+			   << ", max total energy " << max_total_energy
 			   << " )" << std::endl;
-    
+
     // 0 interaction sites
-    
-    if (add_open_state) {	
+
+    if (add_open_state) {
 	HybEnsModel::StateDescription empty_state;
 	if (verbose) {
 	    std::cerr <<"Write open state"<<std::endl;
@@ -162,11 +162,11 @@ main(int argc, char **argv)
 				    binary);
 	}
     }
-    
-    // Indexing for single hybridization 
+
+    // Indexing for single hybridization
     // ----\        /-----
-    //     i1------j1     
-    //     i2------j2     
+    //     i1------j1
+    //     i2------j2
     //  ---/        \--------
 
     if (verbose) {
@@ -186,24 +186,24 @@ main(int argc, char **argv)
 
     if (enum_double_sites) {
 	stopwatch.start("enum_double");
-		
+
 	size_t count_double_states=0;
 	if (verbose) {
 	    std::cerr << "Enumerate double hybridization site states"
 		      << std::endl;
 	}
-	
-	count_double_states = 
+
+	count_double_states =
 	    enumeration.enumerate_double_sites(false);
-	
+
 	if (verbose) {
 	    if (verbose) std::cerr << "\r";
 	    std::cerr <<"Enumerated "<<count_double_states<<" double site states"<<std::endl;
 	}
-    
+
 	stopwatch.stop("enum_double");
     }
-    
+
     stopwatch.stop("enumerate");
 
     cmdline_parser_free(&args_info);
@@ -212,7 +212,7 @@ main(int argc, char **argv)
 
     if (verbose) {
 	stopwatch.print_info(std::cerr);
-    } 
+    }
 
     exit(0);
 }
