dbffdcd2363b2172905d0b73522644d4f32b1987
Sebastian Will <swill@csail.mit.edu>
2023-10-17 15:55:09 +0200

    Extend rikin_pipeline: merge in run instance script

--- src/rikin_pipeline.sh
+++ src/rikin_pipeline.sh
@@ -3,25 +3,31 @@
 # rikin_pipeline.sh
 # Run the Rikin pipeline
 
+## version of this script -- will be included in ouptut
+VERSION="0.9"
+
+
 # -----
 # USAGE
 # -----
 usage() {
     cat <<+++USAGE_MESSAGE
 ============================================================
-rikin_pipeline.sh
+rikin_pipeline.sh ver $VERSION
 
 Run the Rikin pipeline
 
 
-USAGE: rikin_pipeline.sh [-h|--help] [--jobid jobname] [--config file] SEQA SEQB
+USAGE: rikin_pipeline.sh [-h|--help] [options] SEQA SEQB
 
 EXAMPLE CALL:
 rikin_pipeline.sh -j example AAAGGGGGGAAAAAAAGGGUGGGAAAAAAAGGGCGGGAAA CCCGCCC 2>&1 | tee example.out
 
---jobid defines identifier for the job; it must be a valid file and directory name; output is written to files jobname/jobname*
+--outdir output directory
+ 
+--config The given config file defines the pipeline configuration. It must be a valid bash file that is sourced such that it can define environment variables to control the pipeline. These comprise, COMMON_OPTS, ENUM_OPTS. The file is sourced after the global configuration file rikin_pipeline.cfg.
 
---config If the given config file or rikin_pipeline.cfg exists in the current directory, then the it defines the pipeline configuration. It must be a valid bash file that defines COMMON_OPTS and ENUM_OPTS. Attention: this script is executed.
+--dryrun don't run commands and/or write files
 
 SEQA, SEQB: RNA sequences as words over A,C,G,U
 ============================================================
@@ -29,24 +35,29 @@ SEQA, SEQB: RNA sequences as words over A,C,G,U
 +++USAGE_MESSAGE
 }
 
+# -----------------
+# Bin dir
+BINDIR=$(which $0)
+BINDIR=$(dirname $BINDIR)
 
 # -----------------
 # Default arguments
 # -----------------
-#
-
 # jobname and name of output directory
-JOBID="Rikin.results"
+OUTDIR="RIkin-pipeline-results"
+GLOBAL_CONFIGURATION="rikin_pipeline.cfg"
 CONFIGURATION=""
+DRYRUN=false # off
+REUSE=false #off
 
-COMMON_OPTS="--max-hyb-length-diff 8 --span=100"
-ENUM_OPTS="--max-hyb-energy 6 --max-total-energy 10"
+SEQA_NAME=seqA
+SEQB_NAME=seqB
 
 # ---------------
 # Parse arguments
 # ---------------
 
-VALID_ARGS=$(getopt -o hj:c: --long help,jobid:,configuration: -- "$@")
+VALID_ARGS=$(getopt -o hj:c:o: --long help,jobid:,configuration:,outdir:,dryrun,reuse -- "$@")
 if [[ $? -ne 0 ]]; then
     echo "Argument parsing failed."
     usage
@@ -60,6 +71,10 @@ while [ : ]; do
         usage
         exit 0
         ;;
+    -o | --outdir)
+        OUTDIR="$2"
+        shift 2
+        ;;
     -j | --jobid)
         JOBID="$2"
         shift 2
@@ -67,6 +82,15 @@ while [ : ]; do
     -c | --configuration)
         CONFIGURATION="$2"
         shift 2
+        ;;
+    --dryrun)
+        DRYRUN=true
+        shift
+        ;;
+
+    --reuse)
+        REUSE=true
+        shift
         ;;
     --)
         shift;
@@ -105,21 +129,8 @@ fi
 #
 # existence of files ...
 
-
-if [ -e "$JOBID" ] ; then
-    echo "WARNING: output directory $JOBID exists already."
-else
-    mkdir "$JOBID"
-fi
-FILENAME="$JOBID/$JOBID"
-
-
-if [ "$CONFIGURATION" == "" ]; then
-    for CNAME in "rikin_pipeline.cfg"; do
-        if [ -e $CNAME ]; then
-            CONFIGURATION="$CNAME"
-        fi
-    done
+if [ ! -e "$OUTDIR" ] ; then
+    mkdir "$OUTDIR"
 fi
 
 if [ "$CONFIGUATION" != "" ]; then
@@ -133,22 +144,53 @@ if [ "$CONFIGUATION" != "" ]; then
 fi
 
 # ----------
+# Source configuration
+# ----------
+
+# a) global config
+GLOBAL_CONFIGURATION_FULL="$BINDIR/$GLOBAL_CONFIGURATION"
+if [ -e "$GLOBAL_CONFIGURATION_FULL" ] ; then
+    . "$GLOBAL_CONFIGURATION_FULL"
+else
+    echo "WARNING: Cannot find global config at \"$GLOBAL_CONFIGURATION_FULL\"."
+fi
+
+# b) optional config
+if [ "$CONFIGUATION" != "" ]; then
+    . "$CONFIGURATION"
+fi
+
+
+# ----------
 # Print input summary
 # ----------
 
 
 cat <<+++INPUT_SUMMARY
 ============================================================
-RIkin Pipeline
+RIkin Pipeline ver $VERSION
 
-Job ID:      $JOBID
-SeqA:        $SEQA
-SeqB:        $SEQB
-COMMON_OPTS: $COMMON_OPTS
-ENUM_OPTS:   $ENUM_OPTS
+Input:
+  Arguments: $*
+  Job ID:    $JOBID
+  SeqA:      $SEQA
+  SeqB:      $SEQB
+  Outdir:    $OUTDIR
+
+Environment:
+  Working dir: $(pwd)
+  BINDIR=$BINDIR
+  RIkin version $($BINDIR/rikin_enum --version)
+
+Options:
+  COMMON_OPTS="$COMMON_OPTS" (controls rikin_enum and rikin_barriers)
+  ENUM_OPTS="$ENUM_OPTS" (controls rikin_enum)
+  BARRIERS_OPTS="$BARRIERS_OPTS" (controls rikin_barriers)
+  PRUNE_OPTS="$PRUNE_OPTS" (controls rikin_prune)
+  XRATES_OPTS="$XRATES_OPTS" (conrols solving of master equation by rikin_xrates.m)
+  KINETICS_OPTS="$KINETICS_OPTS" (controls plotting by rikin_kinetics.R)
 
 Start date:  $(date)
-Working dir: $(pwd)
 ============================================================
 +++INPUT_SUMMARY
 
@@ -159,13 +201,17 @@ Working dir: $(pwd)
 function call {
     echo
     echo $*
-    $*
+    if ! $DRYRUN ; then
+        $*
+    fi
 }
 
 function tcall {
     echo
     echo $*
-    time $*
+    if ! $DRYRUN ; then
+        time $*
+    fi
 }
 
 function call_redirect {
@@ -173,23 +219,131 @@ function call_redirect {
     shift 1
     echo
     echo $* \> $tgt
-    $* > $tgt
+    if ! $DRYRUN ; then
+        $* > $tgt
+    fi
 }
 
+########################################
+## enumerate and sort
+if [ -e ${OUTDIR}/sorted_states ] \
+   ; then
+    \gzip -f ${OUTDIR}/sorted_states
+fi
 
-call_redirect $FILENAME rikin_enum $SEQA $SEQB --no-double --verbose $ENUM_OPTS $COMMON_OPTS
+if ! $REUSE || [ ! -e ${OUTDIR}/sorted_states.gz ] \
+   ; then
 
-LC_ALL=C sort -g -k1 $FILENAME > $FILENAME.s
+    call_redirect ${OUTDIR}/states $BINDIR/rikin_enum \
+		  $SEQA $SEQB \
+		  $COMMON_OPTS $ENUM_OPTS
 
-call rikin_barriers $SEQA $SEQB -i $FILENAME.s -o $FILENAME.bg --track $FILENAME.barriers_track.gz --no-double --verbose $COMMON_OPTS
+    if [ $? -ne 0 ] ; then
+	echo ERROR: rikin_enum died
+	rm ${OUTDIR}/sorted_states.gz >/dev/null 2>&1
+	exit -1
+    fi
 
-call_redirect $FILENAME.out rikin_prune $FILENAME.bg --pffile $FILENAME.pf  --min-rate 1e-7 --num-out 15 --num-pequ 15 --track $FILENAME.prune_track.gz --verbose --barfile $FILENAME.bar --ratesfile $FILENAME.rates
+    LC_ALL=C sort -k1,1n ${OUTDIR}/states > ${OUTDIR}/sorted_states
+    rm ${OUTDIR}/states
 
-tcall rikin_xrates.m $FILENAME.pf --out $FILENAME.kin --t8 1e15 --mfpts
+    REUSE=false
+fi
 
-call rikin_kinetics.R $FILENAME.kin -o $FILENAME.pdf
+########################################
+## barriers
+if ! $REUSE \
+       || [ ! -e ${OUTDIR}/bg ] \
+       || [ ! -e ${OUTDIR}/barriers_track.gz ] \
+       || [ ! -e ${OUTDIR}/track-ipps-barriers.gz ] \
+   ; then
+
+    if [ -e ${OUTDIR}/sorted_states.gz ] \
+       ; then
+	\gunzip -f ${OUTDIR}/sorted_states.gz
+    fi
+
+    call $BINDIR/rikin_barriers $SEQA $SEQB \
+	 -i ${OUTDIR}/sorted_states \
+	 -o ${OUTDIR}/bg \
+	 --compress-track --track ${OUTDIR}/barriers_track.gz \
+	 --track-ipps=${OUTDIR}/track-ipps-barriers.gz \
+	 $COMMON_OPTS $BARRIERS_OPTS
+    if [ $? -ne 0 ] ; then
+	echo ERROR: rikin_barriers died
+	rm ${OUTDIR}/bg >/dev/null 2>&1
+	exit -1
+    fi
+    REUSE=false
+fi
+
+if [ -e ${OUTDIR}/sorted_states ] \
+   ; then
+    \gzip -f ${OUTDIR}/sorted_states
+fi
+
+########################################
+## prune
+if ! $REUSE \
+       || [ ! -e ${OUTDIR}/out ] \
+       || [ ! -e ${OUTDIR}/pf ] \
+       || [ ! -e ${OUTDIR}/bar ] \
+       || [ ! -e ${OUTDIR}/rates.gz ] \
+       || [ ! -e ${OUTDIR}/track-ipps-prune.gz  ] \
+   ; then
+    call_redirect ${OUTDIR}/out $BINDIR/rikin_prune ${OUTDIR}/bg \
+		  $PRUNE_OPTS \
+		  --pffile ${OUTDIR}/pf --verbose --barfile ${OUTDIR}/bar \
+                  --ratesfile ${OUTDIR}/rates.gz \
+		  --compress-track --track ${OUTDIR}/prune_track.gz \
+		  --track-pps-out=${OUTDIR}/track-ipps-prune.gz \
+		  --track-pps-in=${OUTDIR}/track-ipps-barriers.gz
+    if [ $? -ne 0 ] ; then
+	echo ERROR: rikin_prune died
+	rm ${OUTDIR}/rates.gz >/dev/null 2>&1
+	exit -1
+    fi
+    REUSE=false
+fi
+
+########################################
+## solve master equation
+if ! $REUSE \
+       || [ ! -e ${OUTDIR}/kin ] \
+   ; then
+    tcall $BINDIR/rikin_xrates.m ${OUTDIR}/pf --out ${OUTDIR}/kin $XRATES_OPTS
+    if [ $? -ne 0 ] ; then
+	echo >${OUTDIR}/error "$THE_CALL"
+	echo ERROR: xrates died
+	rm ${OUTDIR}/kin >/dev/null 2>&1
+	exit -1
+    else
+	touch ${OUTDIR}/xrates.done
+	rm ${OUTDIR}/error >/dev/null 2>&1
+    fi
+    REUSE=false
+fi
+
+########################################
+## generate plot
+if ! $REUSE \
+       || [ ! -e ${OUTDIR}/kinetics.pdf ] \
+   ; then
+    call $BINDIR/rikin_kinetics.R ${OUTDIR}/kin -o ${OUTDIR}/kinetics.pdf \
+	 --pps ${OUTDIR}/track-ipps-prune.gz \
+	 --seqA $SEQA --seqB $SEQB --nameA ${SEQA_NAME} --nameB ${SEQB_NAME} \
+         $KINETICS_OPTS
+    if [ $? -ne 0 ] ; then
+	echo ERROR: kinetics.R died
+	rm ${OUTDIR}/kinetics.pdf
+	exit -1
+    fi
+    REUSE=false
+fi
 
 
+## -------------------------------------------
+# 
 cat <<+++END_MESSAGE
 
 
