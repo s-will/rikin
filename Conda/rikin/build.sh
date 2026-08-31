6b065405ad2e54af50d876cde3da7b8024a23559
Sebastian Will <sebastian.will@polytechnique.edu>
2026-08-31 16:22:23 +0200

    Remove conda recipe in favor of bioconda

--- Conda/rikin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# configure script is not committed to git, only configure.ac -- regenerate it
-autoreconf -i
-
-./configure --prefix="${PREFIX}" PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-make -j "${CPU_COUNT}"
-make install
